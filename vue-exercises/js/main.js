ac1ff98beb37ab3f9a92e8e1e9a459795a93ae55
sklalaludek <pattiegolis@gmail.com>
2017-10-19 20:27:05 +0200

    my first try

--- vue-exercises/js/main.js
+++ vue-exercises/js/main.js
@@ -1,7 +1,23 @@
-var data = {
-    output: 'hello world'
-};
-new Vue ({
+//lifecycle hooks
+//directives
+var vm = new Vue ({
     el: '#app',
-    data: data
+    data: {
+        output: 'Hello World',
+        err: 'sth wrong',
+        searchme: '...',
+        count: 4,
+        items: [{
+            todo: "write code"
+        }, {
+            todo: "create website"
+        }, {
+            todo: "load content"
+        }]
+    },
+    created: function () {
+        console.log('Instance created ' + this.output);
+    }
 });
+
+vm.output = "new content";
